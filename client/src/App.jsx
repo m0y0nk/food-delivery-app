1c8aea8e7bfc9efd2d33281b69b32113de377ea0
Mayank <Myank>
2025-05-30 00:12:05 +0530

    Create Navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,11 @@
+import Navbar from "./components/navbar/Navbar"
+
 function App() {
 
   return (
     <>
-      <div className='min-h-[100vh] bg-black text-white'>
-        Hello World 
+      <div className=' text-white'>
+        <Navbar />
       </div>
     </>
   )
